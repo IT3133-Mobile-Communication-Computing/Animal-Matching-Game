263c258b924652a6c12747d831132d6b60ea0413
DilmyPerera <samadiniperera20@gmail.com>
2024-11-23 10:37:33 +0530

    Add AnimalTable Component to the App.js file

--- animal_matching_game/src/App.js
+++ animal_matching_game/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AnimalTable from './components/Animal Table/AnimalTable';
 
 function App() {
   return (
     <div className="App">
-      
+      <AnimalTable/>
     </div>
   );
 }
