e5258ebad672e37256775b737db7717a22dcd786
DilmyPerera <samadiniperera20@gmail.com>
2024-11-23 10:42:33 +0530

    Add state management and click handling to AnimalTable component

--- animal_matching_game/src/components/Animal Table/AnimalTable.js
+++ animal_matching_game/src/components/Animal Table/AnimalTable.js
@@ -1,7 +1,35 @@
 import { animals } from "../db/AnimalsDb";
 import '../Animal Table/AnimalTable.css';
+import React, { useEffect, useState } from 'react'
 
 export default function AnimalTable(props){
+    
+    const [randomAnimal,setRandomAnimal]= useState(null);
+    const [result,setResult]= useState('');
+
+    useEffect(() => {
+        generateRandomAnimal();
+    }, []);
+    
+    const generateRandomAnimal = () => {
+        const randomIndex = Math.floor(Math.random() * 10)+1; 
+        setRandomAnimal(animals[randomIndex]);
+        setResult(''); 
+    };
+
+
+    const handleAnimalClick=(selectedAnimal)=>{
+        if(selectedAnimal === randomAnimal.name){
+            setResult('WIN');
+        }else{
+            setResult('LOSE');
+        }
+
+        setTimeout(() => {
+            generateRandomAnimal(); 
+          }, 1000);
+    };
+    
     return(
         <div className='gameContainer'>
             <table className='game-table'>
